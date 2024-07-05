3cf72bb4ebe3d3a1112ac4c8dd66cd203ffd4a92
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-07-05 12:23:54 +0200

    Update script.js
    Creates function to get results and show message of who won and who lost and show scores

--- script.js
+++ script.js
@@ -8,12 +8,27 @@ let playerScore = 0;
 let computerScore = 0;
 
 function hasPlayerWonTheRound(player, computer) {
-	{ return (
-		(player === "Rock" && computer === "Scissors") ||
-		(player === "Scissors" && computer === "Paper") ||
-		(player === "Paper" && computer === "Rock")
-	);	
+	{
+		return (
+			(player === 'Rock' && computer === 'Scissors') ||
+			(player === 'Scissors' && computer === 'Paper') ||
+			(player === 'Paper' && computer === 'Rock')
+		);
 	}
 }
 
+function getRoundResults(userOption) {
+	const computerResult = getRandomComputerResult();
+	if (hasPlayerWonTheRound(userOption, computerResult)) {
+		playerScore++;
+		return `Player wins! ${userOption} beats ${computerResult}`;
+	} else if (computerResult === userOption) {
+		return `It's a tie! Both choose ${userOption}`;
+	} else {
+		computerScore++;
+		return `Computer wins! ${computerResult} beats ${userOption}`;
+	}
+}
 
+console.log(getRoundResults('Rock'));
+console.log('Player Score: ', playerScore, 'Computer Score: ', computerScore);
