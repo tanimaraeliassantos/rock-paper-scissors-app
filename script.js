57e9b7d32cde5032dfba24ec1f927716034cb314
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-01-04 22:30:15 +0100

    Fix typo

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function getRoundResults(userOption) {
 		playerScore++;
 		return `Player wins! ${userOption} beats ${computerResult}`;
 	} else if (computerResult === userOption) {
-		return `It's a tie! Both choose ${userOption}`;
+		return `It's a tie! Both chose ${userOption}`;
 	} else {
 		computerScore++;
 		return `Computer wins! ${computerResult} beats ${userOption}`;
